90f37a26efc70566bc9fbcc1a4570235bf1d3e60
Ekaterina Kalinina <kalinia_weld@mail.ru>
2024-01-31 22:23:40 +0300

    add new variant of Task4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,5 +1,5 @@
 ﻿int n = 5;
-int[] array = { 2, 10, 5, 4, 8 };
+int[] array = { 2, 10, 5, 30, 8 };
 int i = 0;
 int max = array[0];
 
@@ -10,5 +10,25 @@ while (i < n)
         max = array[i];
     }
     i = i + 1;
+}
+Console.WriteLine(max);
+
+max = array[0];
+for (int j = 0; j < n; j++)
+{
+    if (array[j] > max)
+    {
+        max = array[j];
+    }
+}
+Console.WriteLine(max);
+
+max = array[0];
+foreach (int e in array)
+{
+    if (e > max)
+    {
+        max = e;
+    }
 }
 Console.WriteLine(max);
